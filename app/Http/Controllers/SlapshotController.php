2896263f9eb32332c0a61cb7fce20909a5cea960
jstr-dev <adamcgl@outlook.com>
2025-01-26 17:10:49 +0000

    add slap scontroller

---
+++ app/Http/Controllers/SlapshotController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SlapshotController extends Controller
+{
+    public function getLobbyWebhook(Request $request)
+    {
+
+    }
+}
